406643fd156be0efb1dd709b11a26e39fe19e0fe
Gabriella Aliwa <gabbyaliwa@gmail.com>
2024-02-24 19:38:47 +0300

    Initial commit

---
+++ Greetings.py
@@ -0,0 +1,13 @@
+print("hello there!")
+site_name= "power learn project"
+print(site_name)
+a,b,c=1,2,"Gabby"
+print(a)
+print(b)
+print(c)
+num1= 7
+print(num1,"is of type", type(num1))
+num2= 8.234
+print(num2,"is of type", type(num2))
+num3= 1+2j
+print(num3,"is of type", type(num3))
---
+++ userinput.py
@@ -0,0 +1,7 @@
+name = "Gabriella"
+age = 22
+location = "Thika"
+
+message = "Hello, %s! You are %d years old and live in %s." % (name, age, location)
+print(message)
+
